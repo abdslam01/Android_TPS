bc425a7de9a649b31bbcd9b1fbd257966117fd8e
Abdessalam Bahafid <zomiix246@gmail.com>
2022-02-13 22:55:55 +0100

    Add search in map

---
+++ AndroidExamProject/app/src/main/java/com/example/androidexamproject/helpers/Permission.java
@@ -0,0 +1,26 @@
+package com.example.androidexamproject.helpers;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class Permission {
+    public static boolean checkPermission(Context context, String permission) {
+        return PackageManager.PERMISSION_GRANTED ==
+                ContextCompat.checkSelfPermission(context, permission);
+    }
+
+    private static void askForPermission(Activity activity, String permission, int requestCode) {
+        ActivityCompat.requestPermissions(activity,
+                new String[]{permission}, requestCode);
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+}
--- AndroidExamProject/app/src/main/java/com/example/androidexamproject/ui/HomeFragment.java
+++ AndroidExamProject/app/src/main/java/com/example/androidexamproject/ui/HomeFragment.java
@@ -1,15 +1,21 @@
 package com.example.androidexamproject.ui;
 
+import android.Manifest;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.androidexamproject.R;
+import com.example.androidexamproject.helpers.Permission;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapsInitializer;
@@ -18,25 +24,30 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private View mView;
+    private SearchView searchView;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return getLayoutInflater().inflate(R.layout.activity_maps, null);
+        mView = getLayoutInflater().inflate(R.layout.activity_maps, null);
+        return mView;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        searchView = mView.findViewById(R.id.map_search);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
 
     @Override
@@ -49,6 +60,38 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
         mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList;
+
+                if(!Permission.isNetworkAvailable(getContext())) {
+                    Toast.makeText(getContext(), "Please, check your network", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                if(!location.equals("")) {
+                    Geocoder geocoder = new Geocoder(getContext());
+                    try{
+                        mMap.clear();
+                        addressList = geocoder.getFromLocationName(location, 1);
+                        LatLng latLng = new LatLng(addressList.get(0).getLatitude(), addressList.get(0).getLongitude());
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                    }catch(Exception e) {
+                        Toast.makeText(getContext(), "Location not found", Toast.LENGTH_LONG).show();
+                    }
+                }else
+                    Toast.makeText(getContext(), "Search input is empty", Toast.LENGTH_LONG).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
     }
 
     @Override
